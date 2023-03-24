3de522bb0014e9ede1dc07ec197e953f7892603a
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2023-03-24 23:06:40 +0530

    added pencil drawings

--- client/src/Components/Whiteboard/Canvas.jsx
+++ client/src/Components/Whiteboard/Canvas.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import rough from "roughjs";
 
-const Canvas = () => {
+const roughGenerator = rough.generator();
+
+const Canvas = ({ canvasRef, ctxRef, elements, setElements }) => {
+
+  const [isDrawing, setIsdrawing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctxRef.current = ctx;
+  }, []);
+
+  useLayoutEffect(() => {
+    const roughCanvas = rough.canvas(canvasRef.current);
+    elements.forEach(element => {
+      roughCanvas.linearPath(element.path);
+    });
+  }, [elements]);
+
+  const handleMouseDown = (e) => {
+    const { offsetX, offsetY } = e.nativeEvent;
+    console.log(offsetX, offsetY);
+
+    setElements((prevElements) => [
+      ...prevElements,
+      {
+        type: "pencil",
+        offsetX,
+        offsetY,
+        path: [[offsetX, offsetY]],
+        storke: "black",
+      },
+    ]);
+
+    setIsdrawing(true);
+  }
+
+
+  const handleMouseMove = (e) => {
+    const { offsetX, offsetY } = e.nativeEvent;
+    if (isDrawing) {
+      const { path } = elements[elements.length - 1];
+      const newPath = [...path, [offsetX, offsetY]];
+
+      setElements((prevElements) => 
+        prevElements.map((ele, index) => {
+          if (index === elements.length - 1) {
+            return {
+              ...ele, path: newPath
+            }
+          } else {
+            return ele;
+          }
+        })
+      )
+      console.log(offsetX,offsetY)
+    }
+  }
+
+  const handleMouseUp = (e) => {
+    setIsdrawing(false);
+  }
+
   return (
-    <canvas className='h-100 w-100 border border-3 border-seconday shadow-sm p-3 mb-5 bg-white rounded-2 '>
 
-    </canvas>
-  )
-}
+    <canvas
+      ref={canvasRef}
+      className="h-100 w-100 border border-4 border-seconday shadow-sm p-1 mb-5 bg-white rounded-2 "
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    ></canvas>
+  );
+};
 
-export default Canvas
+export default Canvas;
--- client/src/Pages/Room.jsx
+++ client/src/Pages/Room.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import UndoIcon from '@mui/icons-material/Undo';
 import RedoIcon from '@mui/icons-material/Redo';
 import Canvas from '../Components/Whiteboard/Canvas';
 
+
 const Room = () => {
+
+    const canvasRef = useRef(null);
+    const ctxRef = useRef(null);
     const [tool, setTool] = useState("pencil");
     const [color, setColor] = useState("black");
+    const[elements,setElements]=useState([]);
     const styles = {
         canvasBox: {
-          height: '550px',
-          width:'1500px'
+            height: '550px',
+            width: '1500px'
         },
-      };
+    };
 
     return (
         <div className="row">
@@ -19,36 +24,36 @@ const Room = () => {
             <div className="col-md-10 mx-auto gap-3 px-5 mb-5 d-flex align-items-center justify-content-center">
                 <div className="d-flex col-md-4 justify-content-center gap-5">
                     <div className="d-flex gap-1">
-                        <input className='mt-1' checked={tool ==="pencil"} type="radio" name="tool" id="pencil" value="pencil" onChange={(e) => setTool(e.target.value)} />
+                        <input className='mt-1' checked={tool === "pencil"} type="radio" name="tool" id="pencil" value="pencil" onChange={(e) => setTool(e.target.value)} />
                         <label for="pencil">Pencil</label>
                     </div>
                     <div className="d-flex gap-1">
-                        <input className='mt-1' checked={tool ==="line"}  type="radio" name="tool" id="line" value="line" onChange={(e) => setTool(e.target.value)} />
+                        <input className='mt-1' checked={tool === "line"} type="radio" name="tool" id="line" value="line" onChange={(e) => setTool(e.target.value)} />
                         <label for="line">Line</label>
                     </div>
                     <div className="d-flex gap-1">
-                        <input className='mt-1'  checked={tool ==="rect"} type="radio" name="tool" id="rect" value="rect" onChange={(e) => setTool(e.target.value)} />
+                        <input className='mt-1' checked={tool === "rect"} type="radio" name="tool" id="rect" value="rect" onChange={(e) => setTool(e.target.value)} />
                         <label for="rect">Rectangle</label>
                     </div>
                 </div>
                 <div className="col-md-2 mx-auto">
                     <div className="d-flex align-items-center">
                         <label for="color">Color: </label>
-                        <input type="color" id="color" value={color} className='mt-1 ms-2' onChange={(e)=>setColor(e.target.value)}/>
+                        <input type="color" id="color" value={color} className='mt-1 ms-2' onChange={(e) => setColor(e.target.value)} />
                     </div>
                 </div>
                 <div className="col-md-3 d-flex justify-content-center gap-2">
-                    <button className='btn btn-primary'> <UndoIcon/> Undo</button>
-                    <button className='btn btn-outline-primary'>Redo <RedoIcon/></button>
-                    
-                    
+                    <button className='btn btn-primary'> <UndoIcon /> Undo</button>
+                    <button className='btn btn-outline-primary'>Redo <RedoIcon /></button>
+
+
                 </div>
                 <div className="col-md-2">
                     <button className='btn btn-danger'>Clear Canvas</button>
                 </div>
             </div>
             <div className="col-md-10 mx-auto" style={styles.canvasBox}>
-                <Canvas/>
+                <Canvas canvasRef={canvasRef} ctxRef={ctxRef} elements={elements} setElements={setElements}/>
             </div>
         </div>
     )
